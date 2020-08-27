939624b9f90e2dee981fd4a02f65bb6f5a01ee93
Samuca-sys <samuel.mferreira@hotmail.com>
2020-08-27 20:11:31 -0300

    chore: add project

---
+++ web/src/Components/TeacherItem/index.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+import api from "../../services/api";
+
+import "./styles.css";
+
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post("connections", {
+      user_id: teacher.id,
+    });
+  }
+
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src={teacher.avatar} alt={teacher.name} />
+        <div>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
+        </div>
+      </header>
+
+      <p>{teacher.bio}</p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>{teacher.cost}</strong>
+        </p>
+        <a
+          target="_blank"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}?text=Eu%20tenho%20interesse%20no%20anúncio%20sobre%20ensino%20de%20${teacher.subject}%20no%20Proffy`}
+        >
+          <img src={whatsappIcon} alt="whatsapp" />
+          Entrar em contato
+        </a>
+      </footer>
+    </article>
+  );
+};
+
+export default TeacherItem;
